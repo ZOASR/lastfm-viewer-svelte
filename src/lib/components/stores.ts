507322d6cc671a5e1222742f6e144a2f01d3d6c7
ZOASR <44146518+ZOASR@users.noreply.github.com>
2024-01-02 18:43:29 +0200

    fix: undefined type

--- src/lib/components/stores.ts
+++ src/lib/components/stores.ts
@@ -1,28 +1,28 @@
-import { writable } from "svelte/store";
-import { getLatestTrack, type TrackInfo } from "@repo/utils/lastfm";
+import { writable } from 'svelte/store';
+import { getLatestTrack, type Colors, type TrackInfo } from '@repo/utils/lastfm';
 
-export const lfmvstore = writable();
+export const lfmvstore = writable<{
+	track: TrackInfo | Error;
+	colors: Colors | undefined;
+	loading: boolean;
+}>();
 
 let track: TrackInfo | Error;
 
-export const useLfmv = (
-	user: string,
-	api_key: string,
-	updateInterval?: number
-) => {
+export const useLfmv = (user: string, api_key: string, updateInterval?: number) => {
 	const get = async () => {
 		track = await getLatestTrack(user, api_key);
 		if (track instanceof Error) {
 			lfmvstore.set({
 				track: track,
 				colors: undefined,
-				loading: false,
+				loading: false
 			});
 		} else {
 			lfmvstore.set({
 				track: track,
 				colors: track.colors,
-				loading: false,
+				loading: false
 			});
 		}
 	};
