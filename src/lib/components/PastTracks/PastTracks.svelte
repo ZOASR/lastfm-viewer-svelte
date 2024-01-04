321bd8b58e121c7fdbfecc34cebf0ac04119a932
ZOASR <44146518+ZOASR@users.noreply.github.com>
2024-01-04 17:31:14 +0200

    refactor: used a simpler array method to display past tracks

--- src/lib/components/PastTracks/PastTracks.svelte
+++ src/lib/components/PastTracks/PastTracks.svelte
@@ -35,43 +35,45 @@
 </script>
 
 {#if track && !(track instanceof Error)}
-	{@const pastTracks = cloneArray(track?.pastTracks).splice(1, track?.pastTracks.length)}
-	<div
-		class={styles.pastTracks}
-		style={`color: ${colors?.secondary};background: ${colors?.accent};`}
-	>
-		<LoadingSkeleton className="h-[300px]" fallback={null}>
-			<div
-				class={styles.pastTracks__title}
-				style:background={`rgb(from ${colors?.primary} r g b / 50%)`}
-				style:color={colors?.secondary}
-			>
-				Past tracks
-			</div>
-			{#each pastTracks as track}
-				<div class={styles.pastTracks__track}>
-					<div class="divider m-0.5 h-min" />
-					<div class={styles.scrollable}>
-						<a
-							href={track.url}
-							target="_blank"
-							class={styles.pastTracks__trackTitle}
-							style={`color: ${colors?.secondary}`}
-						>
-							{track.name}
-						</a>
-						<span class={styles.scrollable__artist} style={`color: ${colors?.secondary}`}>
-							<Icon icon="fa-regular:user" />
-
-							{track.artist['#text']}
-						</span>
-						<span class={styles.scrollable__date} style={`color: ${colors?.secondary}`}>
-							<Icon icon="mingcute:calendar-fill" />
-							{track.date['#text']}
-						</span>
-					</div>
+	{#if track.pastTracks}
+		{@const pastTracks = track?.pastTracks?.filter((_, idx) => idx !== 0)}
+		<div
+			class={styles.pastTracks}
+			style={`color: ${colors?.secondary};background: ${colors?.accent};`}
+		>
+			<LoadingSkeleton className="h-[300px]" fallback={null}>
+				<div
+					class={styles.pastTracks__title}
+					style:background={`rgb(from ${colors?.primary} r g b / 50%)`}
+					style:color={colors?.secondary}
+				>
+					Past tracks
 				</div>
-			{/each}
-		</LoadingSkeleton>
-	</div>
+				{#each pastTracks as track}
+					<div class={styles.pastTracks__track}>
+						<div class="divider m-0.5 h-min" />
+						<div class={styles.scrollable}>
+							<a
+								href={track.url}
+								target="_blank"
+								class={styles.pastTracks__trackTitle}
+								style={`color: ${colors?.secondary}`}
+							>
+								{track.name}
+							</a>
+							<span class={styles.scrollable__artist} style={`color: ${colors?.secondary}`}>
+								<Icon icon="fa-regular:user" />
+
+								{track.artist['#text']}
+							</span>
+							<span class={styles.scrollable__date} style={`color: ${colors?.secondary}`}>
+								<Icon icon="mingcute:calendar-fill" />
+								{track.date['#text']}
+							</span>
+						</div>
+					</div>
+				{/each}
+			</LoadingSkeleton>
+		</div>
+	{/if}
 {/if}
