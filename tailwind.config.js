cbe7a40f8b0d3eb28a2e009e220e9eceeea650ab
ZOASR <44146518+ZOASR@users.noreply.github.com>
2025-06-15 17:05:56 +0300

    fix: removed tailwind content config to avoid generating extra styles during dev

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,5 @@ import tailwindConfig from '@lastfm-viewer/tailwind-config';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{svelte,js,ts,jsx,tsx}'],
 	presets: [tailwindConfig]
 };
